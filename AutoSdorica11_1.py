f538e1d55f8fdbdb95e84fff5afde9d5aafe78bd
MulaRoakee <1422323955@qq.com>
2020-08-04 10:32:40 +0800

    添加闪退检测

--- AutoSdorica11_1.py
+++ AutoSdorica11_1.py
@@ -1,7 +1,7 @@
 '''
 Author: MulaRoakee
 Date: 2020-08-04 06:23:05
-LastEditTime: 2020-08-04 09:35:42
+LastEditTime: 2020-08-04 10:28:39
 LastEditors: MulaRoakee
 Description: 自动刷魂能脚本儿 硬要求高 自娱自乐型 泛用性差
 '''
@@ -15,7 +15,9 @@ active_reward = 'active_reward.png'
 fight = 'fight.png'
 arrow1 = 'arrow1.png'
 arrow2 = 'arrow2.png'
-
+see_ghost = 'see_ghost.png'
+challenge = 'challenge.png'
+five = 'five.png'
 
 '''
 description: 这个就厉害了（不是）
@@ -25,7 +27,10 @@ return 无
 '''
 def find_and_click(image):
 
-    '''刚开始先点一下活动奖励下面一些（即聚焦游戏窗口'''
+    # 刚开始先看看是不是见鬼了
+    if_see_ghost()
+
+    # 刚开始先点一下活动奖励下面一些（即聚焦游戏窗口
     if image == active_reward:
         try:
             x, y = au.locateCenterOnScreen(image,confidence=0.9)
@@ -44,6 +49,31 @@ def find_and_click(image):
             
         except:
             print("没找到图片")
+    elif image == challenge:
+        try:
+            x, y = au.locateCenterOnScreen(image,confidence=0.9)
+            au.click(x, y)
+            print("click {0} {1}".format(x,y))
+
+            show_count_down(5)
+            au.click(x,y-300)
+        except:
+            print("没找到图片")
+
+    elif image == fight:
+        try:
+            x, y = au.locateCenterOnScreen(five,confidence=0.9)
+            au.click(x,y)
+            print("click {0} {1}".format(x,y))
+            show_count_down(5)
+        finally:
+            try:
+                x, y = au.locateCenterOnScreen(image,confidence=0.9)
+                au.click(x, y)
+                print("click {0} {1}".format(x,y))
+            except:
+                print("没找到图片")
+
     else:
         try:
             x, y = au.locateCenterOnScreen(image,confidence=0.9)
@@ -97,6 +127,30 @@ def show_count_down(x:int):
         time.sleep(1)
     print()
     
+'''
+description: 触发这个函数说明见鬼了（游戏闪退
+param 无
+return true：游戏闪退
+       false: 游戏正常
+'''  
+def if_see_ghost():
+    try:
+        x, y = au.locateCenterOnScreen(see_ghost,confidence=0.9)
+        au.click(x, y)
+        show_count_down(40)
+        au.click(x,y)
 
+        show_count_down(40)
+        find_and_click(challenge)
+        show_count_down(10)
+
+        while 1:
+            from_list_begin()
+        
+    except:
+        return
+     
+    
 if __name__ == "__main__":
-    from_list_begin()
+    while 1:
+        from_list_begin()
